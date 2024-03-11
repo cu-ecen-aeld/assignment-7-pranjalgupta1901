f44b3b6952e21f2f996427f6234c69902883e063
Pranjal Gupta <prgu8117@aesd-students.colorado.edu>
2024-03-11 05:33:42 +0000

    changed hello.c

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world from pranjalgupta1901\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world from pranjalgupta1901\n");
 }
 
 module_init(hello_init);
